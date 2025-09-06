cfb69506626db5328c96a1148cfdbf39c6696f17
Steven van Beek <steven.van.beek@digitalsurvivalcompany.com>
2025-09-06 09:31:40 +0200

    add

--- playwright.config.ts
+++ playwright.config.ts
@@ -39,22 +39,22 @@ export default defineConfig({
     video: 'on',
 
     // hier pas je de viewport aan
-    viewport: { width: 1920, height: 1080 }
+    viewport: { width: 1920, height: 1200 }
   },
 
   /* Configure projects for major browsers */
   projects: [
     {
       name: 'chromium',
-      use: { ...devices['Desktop Chrome'], viewport: { width: 1920, height: 1080 } },
+      use: { ...devices['Desktop Chrome'], viewport: { width: 1920, height: 1200  } },
     },
     {
       name: 'firefox',
-      use: { ...devices['Desktop Firefox'], viewport: { width: 1920, height: 1080 } },
+      use: { ...devices['Desktop Firefox'], viewport: { width: 1920, height: 1200 } },
     },
     {
       name: 'webkit',
-      use: { ...devices['Desktop Safari'], viewport: { width: 1920, height: 1080 } },
+      use: { ...devices['Desktop Safari'], viewport: { width: 1920, height: 1200 } },
     },
 
     /* Test against mobile viewports. */
